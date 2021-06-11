70a76f4fd79dfba391b5ca344603ee53fb2d777e
TheKingbuildboot <milmadara65@gmail.com>
2021-06-11 11:30:30 -0300

    treewide: Linux 4.14.233

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1225,7 +1225,7 @@ static int mmc_start_request(struct mmc_host *host, struct mmc_request *mrq)
 
 	err = mmc_mrq_prep(host, mrq);
 	if (err)
-		return err;
+		goto power_cycle;
 
 	mmc_host_clk_hold(host);
 	led_trigger_event(host->led, LED_FULL);
